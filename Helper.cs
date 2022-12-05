472b78ad41c85bed414313c3ebfac254d3c12690
Amikan93 <Amikan93@AMIKAN>
2022-12-05 21:38:12 +0600

    Add project files.

---
+++ Helper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Helper
+    {
+        public static string SenttMsg(string to, string subject, string body, out bool result)
+        {
+            try
+            {
+                //Подготовили письмо
+                MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("Fedotov.a@gmail.com", "Fedotov Anton");
+
+            msg.To.Add(to);
+            //msg.CC.Add("");
+            msg.Subject = subject;
+            msg.Priority = MailPriority.High;
+            msg.Body = body;
+            //msg.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials = new NetworkCredential("login", "password");
+
+            //отправляем письмо
+            
+                smtp.Send(msg); //отправь письмо
+                result = true;
+                return "Сообщение отправлено";
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                return ex.Message;
+            }
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();//считывает XAML компоненты, создает компоненты
+
+            NewProject.Title = "NewProject"; //X:Name
+
+            //создаем кнопку
+            //Button btn = new Button();
+            //btn.Content = "BTN";
+            //btn.Width = 50;
+            //btn.Height = 50;
+            //NewProject.Content = btn;
+        }
+
+        private void btnShowAlert_Click(object sender , RoutedEventArgs e) //с кем происходит, что происходит
+        {
+            Button btn = (Button)sender;
+
+            MessageBox.Show("FUCK YOU - "+DateTime.Now);
+        }
+
+        private void BtnSentMsg_OnClick(object sender, RoutedEventArgs e)
+        {
+            bool result = false;
+            lblMessage.Content =  Helper.SenttMsg(tbxTo.Text, tbxSubject.Text, 
+                tbxBody.Text, out result);
+
+            if (!result)
+            {
+                
+                lblMessage.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                
+                lblMessage.Foreground = new SolidColorBrush(Colors.Green);
+            }
+        }
+    }
+}
